cc97a578cc074854c9da61d7d944567a6a095235
EugeneyCh <eugeney@ua.fm>
2023-08-26 15:58:16 +0300

    Update ContactsView.js

--- src/components/pages/ContactsView.js
+++ src/components/pages/ContactsView.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import ContactForm from 'components/ContactForm/contactForm';
 import Filter from 'components/Filter/filter';
 import ListContacts from 'components/ContactList/ContactList';
